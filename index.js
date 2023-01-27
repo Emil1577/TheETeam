2984c2721c430cc38359c4b7654ec94641fd052f
Emil1577 <emilronquillo@gmail.com>
2023-01-26 22:33:54 -0800

    finally figure out a way to generate html

--- index.js
+++ index.js
@@ -5,8 +5,14 @@ const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 
+const managerHtml = require('./src/managerHtml');
+const engineerHtml = require('./src/engineerHtml');
+const internHtml = require('./src/internHtml');
+const teamHtml = require('./src/teamHtml');
 
 const employeeArray = [];
+const internArray = [];
+const engrArray = [];
 
 const mgrQuestions = [
     {
@@ -37,7 +43,7 @@ const engrQuestions = [
     {
         type: 'input',
         message: 'What is your Engineer name?',
-        name:'name'
+        name: 'name'
     },
     {
         type: 'input',
@@ -49,11 +55,7 @@ const engrQuestions = [
         message: 'What is your Engineer email address?',
         name: 'email'
     },
-    {
-        type: 'input',
-        message: 'What is your Engineer office number?',
-        name: 'office'
-    },
+
 
     {
         type: 'input',
@@ -120,14 +122,14 @@ function execMgrQuestions() {
         //generating data based on the answers/response
         .then((mgrData) => {
 
-            console.log(mgrData.name);
+            // console.log(mgrData.name);
 
             const mgrArr = new Manager(mgrData.name, mgrData.id, mgrData.email, mgrData.office);
 
             employeeArray.push(mgrArr);
 
-            console.log(mgrArr);
-            console.log(employeeArray);
+            //  console.log(mgrArr);
+            // console.log(employeeArray);
 
             execAddEmployee()
         })
@@ -140,17 +142,15 @@ function execEngrQuestions() {
         //generating data based on the answers/response
         .then((engrData) => {
 
-            console.log(engrData);
+            //   console.log(engrData);
 
-            const engrArr = new Engineer (engrData.name, engrData.id, engrData.email, engrData.office);
+            const engrArr = new Engineer(engrData.name, engrData.id, engrData.email, engrData.github);
 
-            employeeArray.push(engrArr);
+            engrArray.push(engrArr);
 
-console.log(engrData.Name)
-    
+            // console.log(engrData.Name)
 
-
-            console.log(JSON.stringify(employeeArray));
+            //  console.log(JSON.stringify(employeeArray));
 
             execAddEmployee()
         })
@@ -163,11 +163,11 @@ function execInternQuestions() {
         //generating data based on the answers/response
         .then((internData) => {
 
-            console.log(internData);
+            //  console.log(internData);
 
-            const internArr = new Intern (internData.name, internData.id, internData.email, internData.school);
+            const internArr = new Intern(internData.name, internData.id, internData.email, internData.school);
 
-            employeeArray.push(internArr);
+            internArray.push(internArr);
 
             execAddEmployee()
         })
@@ -180,8 +180,9 @@ function execAddEmployee() {
         //generating data based on the answers/response
         .then((yesNoData) => {
 
-console.log(yesNoData.addEmp)
-            if (yesNoData.addEmp=== 'yes') {
+            //  console.log(yesNoData.addEmp)
+
+            if (yesNoData.addEmp === 'yes') {
                 execAddEmpQuestions()
             }
 
@@ -210,15 +211,99 @@ function execAddEmpQuestions() {
 
         })
 }
-function renderHtml(){
-console.log(employeeArray );
+function renderHtml() {
 
-fs.writeFile('userReadMe.JSON', JSON.stringify(employeeArray), (err) =>
-err ? console.error(err) : console.log('Success!')
-);
+    var html = `
+<!DOCTYPE html>
+<html lang="en" class="vh-100">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Team Profile</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+        crossorigin="anonymous" />
+    <link rel="stylesheet" href="./css/main.css" />
+</head>
+
+<body>
+
+    <header>
+        <div class="col-12 col-md-8 col-lg-12 font-white bg-dark text-light ">
+            <h1>My Team</h1>
+
+        </div>
+
+    </header>
+
+    <main class="section-container d-flex align-items-center">
 
 
+        <main class="col-12 col-md-12 col-lg-12">
+            <div class="container text-center bg-light py-2 rounded-2">
+
+                    <div id="list"></div>
+
+
+                    <div class="row ">
+                        <div  class="box col-10 col-md-6 col-lg-2">
+                        <p id="Manager">Manager
+                            <p> ${employeeArray[0].name}</p>
+                            <p> ${employeeArray[0].id}</p>
+                            <p> ${employeeArray[0].email}</p>
+                            <p> ${employeeArray[0].office}</p>
+                        </div>`
+
+    fs.writeFile('./dist/index.html', html, (err) =>
+        err ? console.error(err) : console.log('Success!'))
+
+    if (internArray.length > 0) {
+        for (let i = 0; i < internArray.length; i++) {
+
+            var internHtml =
+`
+                        <div class="box col-10 col-md-6 col-lg-2">
+                        <p id="Intern">Intern
+                        <p> ${internArray[0].name}</p>
+                        <p> ${internArray[0].id}</p>
+                        <p> ${internArray[0].email}</p>
+                        <p> ${internArray[0].school}</p>
+                            </div>`
+
+       
+        fs.appendFile('./dist/index.html', internHtml, (err) =>
+            err ? console.error(err) : console.log('Success!')) }
+
+
+    }
+
+    if (engrArray.length > 0) {
+        for (let i = 0; i < engrArray.length; i++) {
+
+            var engrHtml =
+                `
+                <div class="box col-10 col-md-6 col-lg-2">
+                  <p id="Engineer">Engineer
+                  <p> ${engrArray[0].name}</p>
+                  <p> ${engrArray[0].id}</p>
+                  <p> ${engrArray[0].email}</p>
+                  <p> ${engrArray[0].github}</p>
+                   </div>`
+                                                  ;
+
+      
+        fs.appendFile('./dist/index.html', engrHtml, (err) =>
+            err ? console.error(err) : console.log('Success!'))  }
+
+
+    }
+
+
+
+console.log(engrArray)
 }
+
 
 execMgrQuestions()
 
